e2599d08d97e7720ba7d09e58121edd31b7db70b
amykim928 <hyeonah928@gmail.com>
2022-03-20 00:33:05 +0900

    Delete else

--- 2주차/14888/14888_python_kimhyeonah.py
+++ 2주차/14888/14888_python_kimhyeonah.py
@@ -11,23 +11,23 @@ def dfs(idx, result):
     if idx == n:
         total.append(result)
         return
-    else:
-        if plus > 0:
-            plus -= 1
-            dfs(idx+1, result+arr[idx])
-            plus += 1
-        if minus > 0:
-            minus -= 1
-            dfs(idx+1, result-arr[idx])
-            minus += 1
-        if multi > 0:
-            multi -= 1
-            dfs(idx+1, result*arr[idx])
-            multi += 1
-        if divi > 0:
-            divi -= 1
-            dfs(idx+1, int(result/arr[idx]))
-            divi += 1
+    
+    if plus > 0:
+        plus -= 1
+        dfs(idx+1, result+arr[idx])
+        plus += 1
+    if minus > 0:
+        minus -= 1
+        dfs(idx+1, result-arr[idx])
+        minus += 1
+    if multi > 0:
+        multi -= 1
+        dfs(idx+1, result*arr[idx])
+        multi += 1
+    if divi > 0:
+        divi -= 1
+        dfs(idx+1, int(result/arr[idx]))
+        divi += 1
             
 n = int(input())
 arr = list(map(int, input().split()))
