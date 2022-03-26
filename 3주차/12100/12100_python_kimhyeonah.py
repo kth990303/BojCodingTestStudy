cf3280bcbeecab373e937323250b86e8decaecec
amykim928 <hyeonah928@gmail.com>
2022-03-27 04:21:55 +0900

    [김현아] 3주차 12100번 과제 제출합니다.

---
+++ 3주차/12100/12100_python_kimhyeonah.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import sys
+from copy import deepcopy
+input = sys.stdin.readline
+
+def up(graph):
+    for j in range(n):
+        last = 0 #더해줄 지난 칸 값
+        pos = 0 #채울 수 있는 행 위치
+        for i in range(n):
+            if graph[i][j]:
+                if last == graph[i][j]:
+                    graph[pos-1][j] = last*2
+                    graph[i][j] = 0
+                    last = 0
+                else:
+                    temp = graph[i][j]
+                    graph[i][j] = 0
+                    graph[pos][j] = temp
+                    last = temp
+                    pos += 1         
+    return graph
+                    
+def down(graph):
+    for j in range(n):
+        last = 0 #더해줄 지난 칸 값
+        pos = n-1 #채울 수 있는 행 위치
+        for i in range(n-1, -1, -1):
+            if graph[i][j]:
+                if last == graph[i][j]:
+                    graph[pos+1][j] = last*2
+                    graph[i][j] = 0
+                    last = 0
+                else:
+                    temp = graph[i][j]
+                    graph[i][j] = 0
+                    graph[pos][j] = temp
+                    last = temp
+                    pos -= 1          
+    return graph
+                
+def left(graph):
+    for i in range(n):
+        last = 0 #더해줄 지난 칸 값
+        pos = 0 #채울 수 있는 열 위치
+        for j in range(n):
+            if graph[i][j]:
+                if last == graph[i][j]:
+                    graph[i][pos-1] = last*2
+                    graph[i][j] = 0
+                    last = 0
+                else:
+                    temp = graph[i][j]
+                    graph[i][j] = 0
+                    graph[i][pos] = temp
+                    last = temp
+                    pos += 1         
+    return graph
+                
+def right(graph):
+    for i in range(n):
+        last = 0 #더해줄 지난 칸 값
+        pos = n-1 #채울 수 있는 열 위치
+        for j in range(n-1, -1, -1):
+            if graph[i][j]:
+                if last == graph[i][j]:
+                    graph[i][pos+1] = last*2
+                    graph[i][j] = 0
+                    last = 0
+                else:
+                    temp = graph[i][j]
+                    graph[i][j] = 0
+                    graph[i][pos] = temp
+                    last = temp
+                    pos -= 1           
+    return graph
+                    
+def dfs(count, graph):
+    global result
+    if count == 5:
+        result = max(result, max(map(max, graph)))
+        return
+    
+    dfs(count+1, up(deepcopy(graph)))
+    dfs(count+1, down(deepcopy(graph)))
+    dfs(count+1, left(deepcopy(graph)))
+    dfs(count+1, right(deepcopy(graph)))
+
+n = int(input())
+graph = [list(map(int, input().split())) for _ in range(n)]
+result = 0
+
+dfs(0, graph)
+print(result)
+
