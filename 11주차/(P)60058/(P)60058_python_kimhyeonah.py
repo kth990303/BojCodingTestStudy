b638526defc83e2d62113eac1f48318811685394
amykim928 <hyeonah928@gmail.com>
2022-05-21 04:44:50 +0900

    [김현아] 11주차 (P)60058번 과제 제출합니다.

---
+++ 11주차/(P)60058/(P)60058_python_kimhyeonah.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def solution(p):
+    def check(p):
+        if p.count('(') == p.count(')'):
+            stack = []
+            for i in p:
+                if i == '(': stack.append(i)
+                else:
+                    if stack: stack.pop()
+                    else: return False
+            return len(stack) == 0
+    
+    if p == '' or check(p): return p
+    
+    u = p[0]
+    v = p[1:]
+    
+    for i in range(2, len(p)+1):
+        if u.count('(') == u.count(')') and v.count('(') == v.count(')'):
+            break
+        u = p[:i]
+        v = p[i:]
+    
+    if check(u):
+        return u + solution(v)
+    
+    result = '(' + solution(v) + ')'
+    u = u[1:len(u)-1]
+    for i in u:
+        if i == '(': result += ')'
+        else: result += '('
+    
+    return result
+
